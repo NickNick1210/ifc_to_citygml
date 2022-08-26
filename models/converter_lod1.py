1ea6bdd80aa307531839986a7d44ffcd941d4ec9
NickNick1210 <nicklas.meyer@ewetel.net>
2022-08-26 10:19:31 +0200

    Neustrukturierung Erstellen der neuen Klassen

---
+++ models/converter_lod1.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+@title: IFC-to-CityGML
+@organization: Jade Hochschule Oldenburg
+@author: Nicklas Meyer
+@version: v0.1 (23.06.2022)
+ ***************************************************************************/
+"""
+
+#####
+
+# Standard-Bibliotheken
+import math
+import sys
+import uuid
+from copy import deepcopy
+from datetime import datetime
+import numpy as np
+
+# IFC-Bibliotheken
+import ifcopenshell
+import ifcopenshell.util.pset
+from ifcopenshell.util import element
+
+# XML-Bibliotheken
+from lxml import etree
+# noinspection PyUnresolvedReferences
+from lxml.etree import QName
+
+# QGIS-Bibliotheken
+from qgis.core import QgsTask
+from qgis.PyQt.QtCore import QCoreApplication
+
+# Geo-Bibliotheken
+from osgeo import ogr
+import sympy
+from sympy import Point3D, Plane, Line
+
+# Plugin
+from .xmlns import XmlNs
+from .mapper import Mapper
+from .transformer import Transformer
+from .utilitiesGeom import UtilitiesGeom
+from .utilitiesIfc import UtilitiesIfc
+
+
+#####
+
+
+class Lod1Converter(QgsTask):
+    """ Model-Klasse zum Konvertieren von IFC-Dateien zu CityGML-Dateien """
